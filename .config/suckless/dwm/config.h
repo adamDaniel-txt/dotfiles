238a668304f44fed06450659fac364bb22cacee8
fdan <fish_kake@proton.me>
2025-06-06 18:09:35 +0800

    change font

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -10,10 +10,10 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata Nerd Font:size=12" };
-static const char dmenufont[]       = "Inconsolata Nerd Font:size=12";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#928374";
 static const char col_gray3[]       = "#bbbbbb";
--- .config/suckless/st/config.h
+++ .config/suckless/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "Inconsolata Nerd Font:pixelsize=12:antialias=true:autohint=true" };
+static char *font2[] = { "monospace:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
