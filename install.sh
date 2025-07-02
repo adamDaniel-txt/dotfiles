eab7b58fc4593e57a941effd90a1c34e84359ca1
fdan <fish_kake@proton.me>
2025-07-02 21:35:50 +0800

    exclusive to deb

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ set -eu -o pipefail # fail on error and report it, debug all lines
 # sudo -n true
 # test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
-pkg_install="apt install"
 USER="fdan" # change to your username
 HOME="/home/$USER"
 
@@ -16,8 +15,10 @@ mkdir -p $HOME/.local/share/themes
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/qutebrowser
 
-echo "Installing the necessary pre-requisites"
-sudo $pkg_install -y git vim zip unzip curl playerctl dunst lf duf pamixer unclutter kitty suckless-tools scrot htop zathura nsxiv mpv neovim cmus zoxide fzf tealdeer gparted ncdu qrencode xclip stow tmux xwallpaper pipewire redshift xcompmgr cron fastfetch qutebrowser preload
+sudo apt update && sudo apt upgrade
+
+echo "Installing the necessary"
+sudo apt install -y git vim zip unzip curl playerctl dunst lf duf pamixer unclutter kitty suckless-tools scrot htop zathura nsxiv mpv neovim cmus zoxide fzf tealdeer gparted ncdu qrencode xclip stow tmux xwallpaper pipewire redshift xcompmgr cron fastfetch qutebrowser preload
 
 echo "Linking dotfiles"
 cd $HOME/.dotfiles/
@@ -25,7 +26,7 @@ stow --adopt .
 git restore .
 
 echo "Installing suckless software"
-sudo $pkg_install -y xorg build-essential dbus-x11 libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev libharfbuzz-dev libjpeg-dev
+sudo apt install -y xorg build-essential dbus-x11 libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev libharfbuzz-dev libjpeg-dev
 
 cd $HOME/.config/suckless/dwm/
 sudo make clean install
@@ -39,10 +40,10 @@ cd $HOME/.config/suckless/sent/
 sudo make clean install
 
 echo "Installing fonts"
-sudo $pkg_install -y fonts-liberation fonts-inconsolata fonts-noto-color-emoji fonts-bebas-neue
+sudo apt install -y fonts-liberation fonts-inconsolata fonts-noto-color-emoji fonts-bebas-neue
 
-cd $HOME
-sudo chown -R $USER:$USER .
+# cd $HOME
+# sudo chown -R $USER:$USER .
 
 echo
 echo "Installation Finish"
