963a5c12b066f1abfeedf53428f7e29120ad2e6f
fdan <fdan@deb>
2025-08-29 21:44:16 +0800

    change browser & etc

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -3,14 +3,14 @@
 /* constants */
 #define TERMINAL "st"
 #define TERMCLASS "St"
-#define BROWSER "qutebrowser"
+#define BROWSER "librewolf"
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
@@ -19,11 +19,11 @@ static const char col_gray2[]       = "#928374";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_green[]       = "#b8bb26";
+static const char col_iwant[]       = "#b8bb26";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_green, col_green },
+	[SchemeSel]  = { col_gray1, col_iwant, col_iwant },
 };
 
 /* tagging */
@@ -88,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_iwant, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *browser[]  = { BROWSER, NULL };
 static const char scratchpadname[] = "scratchpad";
