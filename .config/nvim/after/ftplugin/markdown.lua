ae79bd6490bc688d940516e2e38e0ba6e38396e3
adamDaniel-txt <adam_daniel@tuta.io>
2026-02-17 01:26:19 +0800

    change listing style

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,5 @@
 vim.cmd([[
-	set makeprg=pandoc\ -f\ markdown\ --citeproc\ --bibliography\ ~/.local/share/index.bib\ --template\ eisvogel\ --listing\ -o\ %:r.pdf\ %
+	set makeprg=pandoc\ -f\ markdown\ --citeproc\ --bibliography\ ~/.local/share/index.bib\ --template\ eisvogel\ --highlight-style\ kate\ -o\ %:r.pdf\ %
+	" set makeprg=pandoc\ -f\ markdown\ --citeproc\ --bibliography\ ~/.local/share/index.bib\ --template\ eisvogel\ --listing\ -o\ %:r.pdf\ %
 	" set makeprg=pandoc\ -f\ markdown\ --citeproc\ --bibliography\ ~/.local/share/index.bib\ --template\ eisvogel\ --top-level-division=\"chapter\"\ --highlight-style\ kate\ -o\ %:r.pdf\ %
 ]])
